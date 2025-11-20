75f112b94246a630a0f26edb71fe3b8ea216589d
subin <bistasubeen715@gmail.com>
2025-11-20 09:20:36 -0600

    Added Js for  variable value simulation

---
+++ assets/src/js/VariableWidget.js
@@ -0,0 +1,24 @@
+import { Widget } from "./Widget.js";
+
+export class VariableWidget extends Widget {
+  constructor(container,page) {
+    super(container,page);
+
+    this.valueDisplay = this.container.querySelector(".value-display");
+    this.button = this.container.querySelector(".change-value-btn");
+
+    this.value = 0;
+
+    this.button.addEventListener("click", () => {
+      this.changeValue();
+    });
+  }
+
+  changeValue() {
+    this.value = Math.floor(Math.random() * 10);
+    this.valueDisplay.textContent = this.value;
+
+    this.valueDisplay.classList.add("flash");
+    setTimeout(() => this.valueDisplay.classList.remove("flash"), 300);
+  }
+}
